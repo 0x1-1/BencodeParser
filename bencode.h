c5c8ddbb1cecbc90933a1f8947ff208ce38389fb
0x1-1 <35432275+0x1-1@users.noreply.github.com>
2025-07-10 19:19:13 +0300

    Update bencode.h

--- bencode.h
+++ bencode.h
@@ -1,7 +1,4 @@
 #pragma once
-
-
-
 #include <string>
 #include <filesystem>
 #include <fstream>
@@ -9,7 +6,6 @@
 #include <iostream>
 #include <ctype.h>
 #include <windows.h>
-
 
 namespace Bencode
 {
@@ -42,8 +38,6 @@ namespace Bencode
 
 			std::string raw_value;
 		};
-
-
 	public:
 
 		Parser() = default;
@@ -52,28 +46,17 @@ namespace Bencode
 		void GetHash();
 		void AddFile(const std::filesystem::path& path);
 		void PrintPieces();
-
-		
-
 	private:
 		void Reset();
 		void ProcessChar(const char& c);
-		bool isDigit(const char& c)
-		{
-			if (isdigit(c))
-			{
-				temp = c;
-				m_state = State::Length;
-				return true;
-			}
-			return false;
+		
+		static bool isDigit(const char& c) {
+			return std::isdigit(static_cast<unsigned char>(c));
 		}
-		std::string& GetTemp()
-		{
+		const std::string& GetTemp() const {
 			return temp;
 		}
-		enum class State
-		{
+		enum class State {
 			ReadyForData,
 			Int,
 			Length,
@@ -83,16 +66,9 @@ namespace Bencode
 		};
 
 		int i = 0;
-		std::string temp = "\0";
+		std::string temp;
 		std::vector<BItem> m_items;
 		std::string m_string, m_int, m_hash;
 		State m_state = State::ReadyForData;
 	};
 }
-
-
-
-
-
-
-
